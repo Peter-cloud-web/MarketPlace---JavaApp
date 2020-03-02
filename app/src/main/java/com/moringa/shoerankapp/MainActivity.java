6b69d60e415d11408b60d16db52dfeb786345581
Peter-cloud-web <petergichia165@gmail.com>
2020-03-02 08:29:44 +0300

    Add register activity to manifest

--- app/src/main/java/com/moringa/shoerankapp/MainActivity.java
+++ app/src/main/java/com/moringa/shoerankapp/MainActivity.java
@@ -2,13 +2,36 @@ package com.moringa.shoerankapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.registerApp) Button mRegister;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            ButterKnife.bind(this);
+            mRegister.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+       if ( v == mRegister){
+           openRegister();
+       }
+    }
+
+    private void openRegister() {
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/moringa/shoerankapp/RegisterActivity.java
@@ -0,0 +1,28 @@
+package com.moringa.shoerankapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.moringa.shoerankapp.R;
+
+import butterknife.ButterKnife;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+
+        Button createAccount = (Button) findViewById(R.id.loginNow);
+        EditText name = (EditText) findViewById(R.id.registerAllName);
+        EditText email = (EditText) findViewById(R.id.RegisterEmail);
+        EditText password = (EditText) findViewById(R.id.RegisterPassword);
+
+    }
+}
