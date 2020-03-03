8f73441fd002480791496d6b45ce917b303134ae
Peter-cloud-web <petergichia165@gmail.com>
2020-03-03 11:07:56 +0300

    passing values from adapter to shoeActivity class to be rendered when onClick action is performed

--- app/src/main/java/com/moringa/shoerankapp/shoeActivity.java
+++ app/src/main/java/com/moringa/shoerankapp/shoeActivity.java
@@ -1,4 +1,20 @@
 package com.moringa.shoerankapp;
 
-public class shoeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class shoeActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoe);
+
+        Intent intent = getIntent();
+
+        String name = intent.getExtras().getString("Name");
+        String description = intent.getExtras().getString("Description");
+        String price = intent.getExtras().getString("Price");
+        String image = intent.getExtras().getString("Thumbnail");
+    }
 }
