c7e9d3dfb0d0eb026e5e7312068cc5889ede863c
Peter-cloud-web <petergichia165@gmail.com>
2020-03-19 18:10:04 +0300

    create categories XML file where everything will be loaded from the API using the adapters

--- app/src/main/java/com/moringa/shoerankapp/Constants.java
+++ app/src/main/java/com/moringa/shoerankapp/Constants.java
@@ -4,7 +4,7 @@ package com.moringa.shoerankapp;
 import com.firebase.ui.database.BuildConfig;
 
 public class Constants {
-        public static final String ETSY_BASE_URL = "https://api.bestbuy.com/v1/"
+        public static final String ETSY_BASE_URL = "https://api.bestbuy.com/v1/";
 //        public static final String api_key = BuildConfig.ETSY_API_KEY;
     }
 
