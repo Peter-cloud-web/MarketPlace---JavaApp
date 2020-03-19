e29bca4031684ca17949caeeb46df5149e93d116
Peter-cloud-web <petergichia165@gmail.com>
2020-03-19 17:17:24 +0300

    Using the http annotation GET to fetch products and categoires from the API

--- app/src/main/java/com/moringa/shoerankapp/EtsyApi.java
+++ app/src/main/java/com/moringa/shoerankapp/EtsyApi.java
@@ -6,7 +6,7 @@ import retrofit2.http.GET;
 public interface EtsyApi {
 
     //products get request
-    @GET("products?apiKey=BxaYUpnn8AUTWp69vYwpDi1V&format=json"
+    @GET("products?apiKey=BxaYUpnn8AUTWp69vYwpDi1V&format=json")
     Call<BestBuyProductSearchResponse> getProducts(
 //            @Query("api key") String query api_key
     );
