faaa2208c538bcb55b2fa8aa1d1faf0df2a1859a
Peter-cloud-web <petergichia165@gmail.com>
2020-04-02 15:58:08 +0300

    onTouch returns boolean value if event was handled successfully

--- app/src/main/java/com/moringa/shoerankapp/Network/BestbuyApi.java
+++ app/src/main/java/com/moringa/shoerankapp/Network/BestbuyApi.java
@@ -12,12 +12,12 @@ public interface BestbuyApi {
     //products get request
     @GET("products?apiKey=BxaYUpnn8AUTWp69vYwpDi1V&format=json")
     Call<BestBuyProductSearchResponse> getProducts(
-//            @Query("api key") String query api_key
+
     );
 
     //categories get request
     @GET("categories?apiKey=BxaYUpnn8AUTWp69vYwpDi1V&format=json")
     Call<BestbuyCategoriesResponse> getCategories(
-//            @Query("api key") String query api_key
+
     );
 }
